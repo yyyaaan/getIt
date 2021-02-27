27c96f55281fbdcf107025a38700d47148e1962f
Yan (csc) <yan.csc@yan.fi>
2021-02-27 20:55:52 +0000

    minor correction

--- scheduled.R
+++ scheduled.R
@@ -16,7 +16,6 @@ job_hlt <- "us"
 job_mrt <- "fi"
 job_qr  <- "csc"
 
-
 ### by date (currently a 4-day loop)
 controller   <<- as.numeric(Sys.Date()) %% 4
 def_interval <<- 20:35
@@ -60,47 +59,47 @@ get_exchange_rate()
 if(grepl(job_mrt, this_server)){
   suppressMessages(source("./src/mrt01.R"))
   logger("START MRT01", mrt_fu_dates)
-  # start_mrt01(mrt_fu_dates, mrt_fu_nights, mrt_fu_hotels)
-  # save_data_mrt01(paste0("mrt01_", gsub("-", "", Sys.Date())))
-  # suppressMessages(serve_mrt01())
+  start_mrt01(mrt_fu_dates, mrt_fu_nights, mrt_fu_hotels)
+  save_data_mrt01(paste0("mrt01_", gsub("-", "", Sys.Date())))
+  suppressMessages(serve_mrt01())
 }
 
 
 if(grepl(job_fsh, this_server)){
   suppressMessages(source("./src/fsh01.R"))
   logger("START FSH01", fsh_fu_dates)
-  # start_fsh01(fsh_fu_dates, fsh_fu_nights, fsh_fu_hotels)
-  # save_data_fsh01(paste0("fsh01_", gsub("-", "", Sys.Date())))
+  start_fsh01(fsh_fu_dates, fsh_fu_nights, fsh_fu_hotels)
+  save_data_fsh01(paste0("fsh01_", gsub("-", "", Sys.Date())))
 }
 
 
 if(grepl(job_qr, this_server)){
   suppressMessages(source("./src/qr01.R"))
   logger("START QR01", qr_fu_dates)
-  # start_qr01(qr_fu_deps, qr_fu_dests, qr_fu_dates, qr_the_days)
-  # save_data_qr01(paste0("qr01_", gsub("-", "", Sys.Date())))
+  start_qr01(qr_fu_deps, qr_fu_dests, qr_fu_dates, qr_the_days)
+  save_data_qr01(paste0("qr01_", gsub("-", "", Sys.Date())))
 }
 
 if(grepl(job_acr, this_server)){
   suppressMessages(source("./src/acr01.R"))
   logger("START ACR01", acr_fu_dates)
-  # start_acr01(acr_fu_dates, acr_fu_nights, acr_fu_hotels)
-  # save_data_acr01(paste0("acr01_", gsub("-", "", Sys.Date())))
+  start_acr01(acr_fu_dates, acr_fu_nights, acr_fu_hotels)
+  save_data_acr01(paste0("acr01_", gsub("-", "", Sys.Date())))
 }
 
 if(grepl(job_hlt, this_server)){
   suppressMessages(source("./src/hlt01.R"))
   logger("START HLT01", hlt_fu_dates)
-  # start_hlt01(hlt_fu_dates, hlt_fu_nights, hlt_fu_hotels)
-  # save_data_hlt01(paste0("hlt01_", gsub("-", "", Sys.Date())))
+  start_hlt01(hlt_fu_dates, hlt_fu_nights, hlt_fu_hotels)
+  save_data_hlt01(paste0("hlt01_", gsub("-", "", Sys.Date())))
 }
 
 if(grepl(job_ay, this_server)){
   suppressMessages(source("./src/ay01.R"))
   def_interval <<- 69:129
   logger("START AY01 sp", controller )
-  # start_ay01_special("Tahiti", controller)
-  # save_data_ay01(paste0("ay01_", gsub("-", "", Sys.Date())))
+  start_ay01_special("Tahiti", controller)
+  save_data_ay01(paste0("ay01_", gsub("-", "", Sys.Date())))
 }
 
 
