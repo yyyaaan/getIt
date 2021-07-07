50104bca648197e5d356ad800b615e69d25abed5
yyyaaan <yan@yan.fi>
2021-07-07 19:49:01 +0000

    fix for acr01 (idle0 -> idle2)

--- src/acr01.js
+++ src/acr01.js
@@ -1,10 +1,10 @@
-const params  = ['https://all.accor.com/ssr/app/accor/rates/9924/index.en.shtml?dateIn=2021-02-06&nights=4&compositions=2&stayplus=false', 'acr_tmpx'];
+const params  = ['https://all.accor.com/ssr/app/accor/rates/9924/index.en.shtml?dateIn=2022-02-06&nights=4&compositions=2&stayplus=false', 'acr_tmpx'];
 const req_url   = params[0];
 const req_name  = params[1];
 const out_text  = params[2];
 const puppeteer = require('puppeteer');
 const filesave  = require('fs');
-const wait_opts = {waitUntil: 'networkidle0'};
+const wait_opts = {waitUntil: 'networkidle2'};
 const max_time  = 19000;
 const ua_string = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36';
 const exe_start = new Date();
@@ -36,6 +36,13 @@ out.push("\n<qurl>" + req_url + '</qurl>\n<timestamp>' + (exe_start.toISOString(
     await page.goto(req_url, wait_opts);
     await page.waitForSelector('.default__main');
     
+    // cookie setting confirmation
+    // let status = await page.$("#onetrust-close-btn-container > button > strong");
+    // if(status !== null){
+    //  await page.click("#onetrust-close-btn-container > button > strong");
+    // }
+
+    
     // determing availability
     var availability = await page.evaluate(() => document.querySelector('.default__main').innerText);
     if(availability.toLowerCase().search('sorry') >= 0) {
@@ -54,6 +61,7 @@ out.push("\n<qurl>" + req_url + '</qurl>\n<timestamp>' + (exe_start.toISOString(
   } catch (e) {	
     out[0] = 'error';
     out.push(e);
+    await page.screenshot({path: './cache/a_acr01.png'});
     filesave.writeFile('./cache/' + req_name + '.pp', out.join(), function(err) {}); 
   }
   
